163518cc58b650f3a20c254551b26410258e69db
Ars9-tech <aradhanasoni214@gmail.com>
2026-07-12 00:05:57 +0530

    Add myPow function to calculate x raised to n
    Implement recursive power function for double values with handling for negative exponents.

---
+++ Arrays/Problem_50.cpp
@@ -0,0 +1,32 @@
+Pow(x,n)
+--------
+
+class Solution {
+public:
+    double helper(double x, long long n){  // base x, power n
+        if(n==0) 
+            return 1;
+
+        double temp = helper(x, n/2); //recursive calling x^n/2
+        if (n%2 == 0) 
+            return temp*temp;  // n is even
+        else 
+            return x*temp*temp; // n is odd
+    }
+    double myPow(double x, int n) {
+        long long exp = abs((long long)n);  // this line handles negative exponent safely 
+        double result = helper(x, exp);
+        if(n<0)
+            return 1.0/result;
+        
+        return result;
+    }
+        
+    
+};
+
+
+//Space Complexity = O(log n)
+//Time Complexity = O(log n)
+
+  
