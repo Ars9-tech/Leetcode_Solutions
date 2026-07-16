761b7b82768ca3fe3f3479b61d6dba555037664c
Ars9-tech <aradhanasoni214@gmail.com>
2026-07-16 23:30:58 +0530

    Create Problem_1143.cpp

---
+++ Dynamic Programming/Problem_1143.cpp
@@ -0,0 +1,30 @@
+//LONGEST COMMON SUBSEQUENCE
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+        //vector<vector<int>> L(m+1, vector<int>(n+1));
+        int L[1001][1001];
+
+    
+        for(int i = 0; i<=m; i++){
+            L[i][0]=0;
+        }
+        for(int j = 0; j<=n; j++){
+            L[0][j]=0;
+        }
+        for(int i = 1 ; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if (text1[i-1]==text2[j-1]){
+                    L[i][j] = L[i-1][j-1]+1;
+                }
+                else
+                    L[i][j] = max(L[i-1][j], L[i][j-1]);
+            }
+            
+        }
+        return L[m][n];
+        
+    }
+};
