337981e85bd7c3fa6591f25381fb5f41aa3f9e31
Ars9-tech <aradhanasoni214@gmail.com>
2026-07-16 23:22:17 +0530

    Create problem_121.cpp
    Implement solution for the best time to buy and sell stock problem.

---
+++ Arrays/problem_121.cpp
@@ -0,0 +1,21 @@
+//Best time to sell and buy stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy= prices[0];
+        int profit = 0;
+        //int n = prices.size();
+        for(int i=1; i<prices.size();i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }
+            else if(prices[i]-buy>profit){
+                profit = prices[i]-buy;
+            }
+
+
+        }
+        return profit;
+        
+    }
+};
